d8c5b83a3167e59dd5e36575f119c74d2b28a904
Kai Neuhaus <email.kai.neuhaus@gmail.com>
2020-09-14 22:41:00 +0100

    Change how to capture different data content for different configurations.

--- OCT_convert.py
+++ OCT_convert.py
@@ -1,3 +1,18 @@
+"""
+Testing and usage:
+
+run this function at the end of this file ...
+
+mat_data = OCTtoMATraw('test.oct')
+
+Calling this function here directly is for testing.
+The last command can be removed and then this OCT_converter can be imported:
+
+import OCT_converter
+
+mat_data OCT_converter.OCTtoMATraw('test.oct')
+
+"""
 import numpy as np
 from scipy.fftpack import fft,ifft
 from scipy.interpolate import interp1d
@@ -13,10 +28,10 @@ formatwarning_orig = warnings.formatwarning
 warnings.formatwarning = lambda message, category, filename, lineno, line=None: \
     formatwarning_orig(message, category, filename='', lineno='', line='')
 
-# Collection of functions to convert from different OCT file configurations
 def shorten_dict_keys( in_dict ):
     '''
     The MAT format does not allow key lengths larger 31 characters.
+    Remove special characters '@' and '#' from key names.
     This function returns a new dict looping over all keys and tries to reduce the string length.
     '''
     out_dict = {}
@@ -62,54 +77,44 @@ def OCTtoMATraw(oct_filename):
             mat_data['Header']['DataFileDict'][inoct_filename] = shorten_dict_keys(file_object)
 
 
-        # Check if first spectral data has ScanRegionStart0 
-        if 'ScanRegionStart0' in [k for k in mat_data['Header']['DataFileDict']['Spectral0'].keys()]:
-            # use parameters including Spectral0
-            use_Spectral0 = True
-            S0arr_type =    (mat_data['Header']['DataFileDict']['Spectral0']['Type'])
-            S0SizeZ    = int(mat_data['Header']['DataFileDict']['Spectral0']['SizeZ'])
-            S0SizeX    = int(mat_data['Header']['DataFileDict']['Spectral0']['SizeX'])
-            S0bpp      = int(mat_data['Header']['DataFileDict']['Spectral0']['BytesPerPixel'])
-            S0ar_start = int(mat_data['Header']['DataFileDict']['Spectral0']['ApoRegionStart0'])
-            S0ar_end   = int(mat_data['Header']['DataFileDict']['Spectral0']['ApoRegionEnd0'])
-            S0sr_start = int(mat_data['Header']['DataFileDict']['Spectral0']['ScanRegionStart0'])
-            S0sr_end   = int(mat_data['Header']['DataFileDict']['Spectral0']['ScanRegionEnd0'])
-            S0dtype    = python_dtypes[S0arr_type][is_signed][S0bpp]
-        else:
-            # use parameters Spectral0 special only Apo data
-            use_Spectral0 = False
-            S0arr_type =    (mat_data['Header']['DataFileDict']['Spectral0']['Type'])
-            S0SizeZ    = int(mat_data['Header']['DataFileDict']['Spectral0']['SizeZ'])
-            S0SizeX    = int(mat_data['Header']['DataFileDict']['Spectral0']['SizeX'])
-            S0bpp      = int(mat_data['Header']['DataFileDict']['Spectral0']['BytesPerPixel'])
-            S0ar_start = int(mat_data['Header']['DataFileDict']['Spectral0']['ApoRegionStart0'])
-            S0ar_end   = int(mat_data['Header']['DataFileDict']['Spectral0']['ApoRegionEnd0'])
-            S0dtype    = python_dtypes[S0arr_type][is_signed][S0bpp]
+        S0arr_type = (mat_data['Header']['DataFileDict']['Spectral0']['Type'])
+        S0SizeZ = int(mat_data['Header']['DataFileDict']['Spectral0']['SizeZ'])
+        S0SizeX = int(mat_data['Header']['DataFileDict']['Spectral0']['SizeX'])
+        S0bpp = int(mat_data['Header']['DataFileDict']['Spectral0']['BytesPerPixel'])
+        S0ar_start = int(mat_data['Header']['DataFileDict']['Spectral0']['ApoRegionStart0'])
+        S0ar_end = int(mat_data['Header']['DataFileDict']['Spectral0']['ApoRegionEnd0'])
+        S0dtype = python_dtypes[S0arr_type][is_signed][S0bpp]
 
-            # use parameters Spectral1 for all others
-            S1arr_type = mat_data['Header']['DataFileDict']['Spectral1']['Type']
-            S1SizeZ    = int(mat_data['Header']['DataFileDict']['Spectral1']['SizeZ'])
-            S1SizeX    = int(mat_data['Header']['DataFileDict']['Spectral1']['SizeX'])
-            S1bpp      = int(mat_data['Header']['DataFileDict']['Spectral1']['BytesPerPixel'])
-            S1ar_start = int(mat_data['Header']['DataFileDict']['Spectral1']['ApoRegionStart0'])
-            S1ar_end   = int(mat_data['Header']['DataFileDict']['Spectral1']['ApoRegionEnd0'])
-            S1sr_start = int(mat_data['Header']['DataFileDict']['Spectral1']['ScanRegionStart0'])
-            S1sr_end   = int(mat_data['Header']['DataFileDict']['Spectral1']['ScanRegionEnd0'])
-            S1dtype    = python_dtypes[S1arr_type][is_signed][S1bpp]
+        S1arr_type  = mat_data['Header']['DataFileDict']['Spectral1']['Type']
+        S1SizeZ     = int(mat_data['Header']['DataFileDict']['Spectral1']['SizeZ'])
+        S1SizeX     = int(mat_data['Header']['DataFileDict']['Spectral1']['SizeX'])
+        S1bpp       = int(mat_data['Header']['DataFileDict']['Spectral1']['BytesPerPixel'])
+        S1sr_start  = int(mat_data['Header']['DataFileDict']['Spectral1']['ScanRegionStart0'])
+        S1sr_end    = int(mat_data['Header']['DataFileDict']['Spectral1']['ScanRegionEnd0'])
+        S1dtype     = python_dtypes[S1arr_type][is_signed][S1bpp]
 
         for item in zf.filelist:
             print(item.filename)
 
-            if 'Spectral0' in item.filename and not use_Spectral0:
-                # Spectral0 is all apodata
-                data = np.frombuffer(zf.read(item.filename), dtype=(S0dtype,[S0SizeX,S0SizeZ]))[0]
+            if 'Spectral0' in item.filename and mat_data['Header']['DataFileDict']['Spectral0'].get('ScanRegionStart0'):
+                S0sr_start = int(mat_data['Header']['DataFileDict']['Spectral0']['ScanRegionStart0'])
+                S0sr_end = int(mat_data['Header']['DataFileDict']['Spectral0']['ScanRegionEnd0'])
+                data = np.frombuffer(zf.read(item.filename), dtype=(S0dtype, [S0SizeX, S0SizeZ]))[0]
+                mat_data['Spectral'].append(data[S0sr_start:S0sr_end, :])
+                mat_data['Spectral_apo'].append(data[S0ar_start:S0ar_end, :])
+            elif 'Spectral0' in item.filename:
+                data = np.frombuffer(zf.read(item.filename), dtype=(S0dtype, [S0SizeX, S0SizeZ]))[0]
                 mat_data['Spectral_apo'].append(data)
-            elif 'Spectral' in item.filename and use_Spectral0:
-                data = np.frombuffer(zf.read(item.filename), dtype=(S0dtype,[S0SizeX,S0SizeZ]))[0]
-                # store apo data separately
-                apo_data = data[S0ar_start:S0ar_end,:]
-                data = data[S0sr_start:S0sr_end,:]
-                mat_data['Spectral_apo'].append(apo_data)
+
+            elif 'Spectral1' in item.filename and mat_data['Header']['DataFileDict']['Spectral1'].get('ApoRegionStart0'):
+                S1ar_start = int(mat_data['Header']['DataFileDict']['Spectral1']['ApoRegionStart0'])
+                S1ar_end = int(mat_data['Header']['DataFileDict']['Spectral1']['ApoRegionEnd0'])
+                data = np.frombuffer(zf.read(item.filename), dtype=(S1dtype,[S1SizeX,S1SizeZ]))[0]
+                mat_data['Spectral_apo'].append(data[S1ar_start:S1ar_end,:])
+                mat_data['Spectral'].append(data[S1sr_start:S1sr_end,:])
+
+            elif 'Spectral' in item.filename and not('Spectral0' in item.filename):
+                data = np.frombuffer(zf.read(item.filename), dtype=(S1dtype,[S1SizeX,S1SizeZ]))[0]
                 mat_data['Spectral'].append(data)
 
             elif 'Chirp' in item.filename:
@@ -119,15 +124,19 @@ def OCTtoMATraw(oct_filename):
                 py_dtype = python_dtypes[arr_type][bpp]
                 data = np.frombuffer(zf.read(item.filename),dtype=(py_dtype, SizeZ))
                 mat_data['Chirp'] = data
-            # elif 'ApodizationSpectrum' in item.filename:
-            #     mat_data['ApodizationSpectrum'] = item.filename
+            elif 'ApodizationSpectrum' in item.filename:
+                arr_type = mat_data['Header']['DataFileDict']['ApodizationSpectrum']['Type']
+                SizeZ = int(mat_data['Header']['DataFileDict']['ApodizationSpectrum']['SizeZ'])
+                bpp = int(mat_data['Header']['DataFileDict']['ApodizationSpectrum']['BytesPerPixel'])
+                py_dtype = python_dtypes[arr_type][bpp]
+                data = np.frombuffer(zf.read(item.filename),dtype=(py_dtype, SizeZ))
+                mat_data['ApodizationSpectrum'] = data
             # elif 'OffsetErrors' in item.filename:
             #     mat_data['OffsetErrors'] = zf.read(item.filename)
 
     from scipy.io.matlab import savemat
-    savemat(re.split('.[oO][cC][tT]',oct_filename)[0]+'.mat', mat_data)
+    savemat(re.split('\.[oO][cC][tT]',oct_filename)[0]+'.mat', mat_data)
 
     return mat_data
 
 mat_data = OCTtoMATraw('test.oct') # see OCT_reader_demo.py to retrieve test.oct
-
